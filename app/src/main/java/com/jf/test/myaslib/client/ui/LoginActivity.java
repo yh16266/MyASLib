27494e385a6179acb1728c9f06fc9b8b100afa58
haozi <263667227@qq.com>
2016-03-07 11:43:19 +0800

    cmp home activity

--- app/src/main/java/com/jf/test/myaslib/client/ui/LoginActivity.java
+++ app/src/main/java/com/jf/test/myaslib/client/ui/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.jf.test.myaslib;
+package com.jf.test.myaslib.client.ui;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -25,6 +25,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.jf.test.myaslib.R;
 
 import java.util.ArrayList;
 import java.util.List;
