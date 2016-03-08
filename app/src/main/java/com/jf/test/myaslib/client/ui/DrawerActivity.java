621f5faeca64836f22f8764c33241ee9541590f1
haozi <263667227@qq.com>
2016-03-08 18:01:41 +0800

    toolsbar test new

---
+++ app/src/main/java/com/jf/test/myaslib/client/ui/DrawerActivity.java
@@ -0,0 +1,15 @@
+package com.jf.test.myaslib.client.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jf.test.myaslib.R;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+    }
+}
--- app/src/main/java/com/jf/test/myaslib/client/ui/ToolsbarActivity.java
+++ app/src/main/java/com/jf/test/myaslib/client/ui/ToolsbarActivity.java
@@ -3,6 +3,8 @@ package com.jf.test.myaslib.client.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.jf.test.myaslib.R;
 
@@ -14,7 +16,30 @@ public class ToolsbarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_toolsbar);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         toolbar.setTitle("MD toolbar 测试");
-        //setSupportActionBar(toolbar);
+        toolbar.setSubtitle("副标题");
+        toolbar.setLogo(R.drawable.ic_menu_slideshow);
+        toolbar.setNavigationIcon(android.R.drawable.ic_menu_crop);
+        //toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+        //    @Override
+        //    public boolean onMenuItemClick(MenuItem item) {
+        //        return true;
+        //    }
+        //});
+        //toolbar.inflateMenu(R.menu.activity_toolsbar_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_toolsbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
