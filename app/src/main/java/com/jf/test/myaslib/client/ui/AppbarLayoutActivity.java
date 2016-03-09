35d884219874b246aa0c914d62fe0b8735043fa6
haozi <263667227@qq.com>
2016-03-09 18:48:13 +0800

    toolsbar recycleview update

--- app/src/main/java/com/jf/test/myaslib/client/ui/AppbarLayoutActivity.java
+++ app/src/main/java/com/jf/test/myaslib/client/ui/AppbarLayoutActivity.java
@@ -2,14 +2,21 @@ package com.jf.test.myaslib.client.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.internal.view.menu.ActionMenuItemView;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.jf.test.myaslib.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AppbarLayoutActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private AppbarLayoutAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +25,28 @@ public class AppbarLayoutActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("MD AppbarLayoutActivity 测试");
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        initView();
+        initData();
     }
 
     private void initView(){
         this.recyclerView = (RecyclerView) findViewById(R.id.rvToDoList);
+    }
+
+    private void initData(){
+        List<String> mData = new ArrayList<>();
+        for (int i=0;i<20;i++){
+            mData.add("test"+i);
+        }
+        mAdapter = new AppbarLayoutAdapter(this,mData);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(mAdapter);
     }
 }
--- app/src/main/java/com/jf/test/myaslib/client/ui/AppbarLayoutAdapter.java
+++ app/src/main/java/com/jf/test/myaslib/client/ui/AppbarLayoutAdapter.java
@@ -1,7 +1,16 @@
 package com.jf.test.myaslib.client.ui;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateFormat;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.jf.test.myaslib.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,22 +18,45 @@ import java.util.List;
 /**
  * Created by Admin on 2016/3/9.
  */
-public class AppbarLayoutAdapter extends RecyclerView.Adapter {
+public class AppbarLayoutAdapter extends RecyclerView.Adapter<AppbarLayoutAdapter.ViewHolder> {
 
+    private Context mContext;
     private List<String> mData = new ArrayList<>();
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public AppbarLayoutAdapter(Context mContext, List<String> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.appbar_recyclerview_itme, null);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        view.setLayoutParams(lp);
+        return new ViewHolder(view);
+    }
 
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.txv_title.setText(mData.get(position));
+        holder.txv_time.setText(DateFormat.format("yyyy-MM-dd",System.currentTimeMillis()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView txv_title;
+        public TextView txv_time;
+
+        public ViewHolder(View view){
+            super(view);
+            txv_title = (TextView)view.findViewById(R.id.txv_title);
+            txv_time = (TextView)view.findViewById(R.id.txv_time);
+        }
+
     }
 }
